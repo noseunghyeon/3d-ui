eafdb3d24d51da098db2103fde5d1249ba5b213f
noseunghyeon <ro981009@gmail.com>
2024-11-20 14:00:05 +0900

    1400

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ModelView from "./ModelView";
 import { yellowImg } from "./../utils/index";
 
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models, sizes } from "./../constants/index";
+import { animateGsapTimeline } from "./../constants/animation";
 
 const Model = () => {
   useGSAP(() => {
@@ -29,24 +31,54 @@ const Model = () => {
 
   // rotation states
   const [smallRotation, setSmallRotation] = useState(0);
-  const [largeRotation, setlargeRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 1,
+      });
+    }
+  }, [size]);
+
+  useEffect(() => {
+    if (size === "small") {
+      animateGsapTimeline(tl, large, largeRotation, "#view1", "#view2", {
+        transform: "translateX(0)",
+        duration: 1,
+      });
+    }
+  }, [size]);
 
   return (
-    <section className="">
+    <section className="py-10">
       <div className="screen-max-width">
-        <h1 id="heading" className="section-heading">
+        <h1 id="heading" className="section-heading px-4 pt-8">
           Take a closer look.
         </h1>
       </div>
 
       <div className="flex flex-col items-center mt-5">
-        <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative bg-slate-600">
+        <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
           <ModelView
             index={1}
             groupRef={small}
             controlRef={cameraControlSmall}
             setRotationSize={setSmallRotation}
             gsapType="view1"
+            item={model}
+            size={size}
+          />
+
+          <ModelView
+            index={2}
+            groupRef={large}
+            controlRef={cameraControlLarge}
+            setRotationSize={setLargeRotation}
+            gsapType="view2"
             item={model}
             size={size}
           />
@@ -65,6 +97,36 @@ const Model = () => {
           >
             <View.Port />
           </Canvas>
+        </div>
+        <div className="mx-auto w-full">
+          <p className="text-sm font-light text-center mb-5">{model.title}</p>
+          <div className="flex-center">
+            <ul className="color-container">
+              {models.map((item, idx) => (
+                <li
+                  key={idx}
+                  className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                  style={{ backgroundColor: item.color[0] }}
+                  onClick={() => setModel(item)}
+                ></li>
+              ))}
+            </ul>
+            <button className="size-btn-container">
+              {sizes.map(({ label, value }) => (
+                <span
+                  key={label}
+                  className="size-btn"
+                  style={{
+                    backgroundColor: size === value ? "#fff" : "transparent",
+                    color: size === value ? "#000" : "#fff",
+                  }}
+                  onClick={() => setSize(value)}
+                >
+                  {label}
+                </span>
+              ))}
+            </button>
+          </div>
         </div>
       </div>
     </section>
